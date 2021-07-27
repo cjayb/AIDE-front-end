3b2c3ceef92beff88f6ef04ebe66c6990ec72918
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2021-07-27 16:48:45 +0100

    fix some spacing issues

--- src/components/ClinicalReview/Tasks.vue
+++ src/components/ClinicalReview/Tasks.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container>
+    <v-container class="pa-0">
         <v-list-item>
             <v-list-item-content>
                 <v-list-item-title class="text-h6">Work List</v-list-item-title>
