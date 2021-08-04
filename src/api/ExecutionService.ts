109912d2f78b9fc86eba4a272367eafac1a057ed
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2021-08-04 17:35:43 +0100

    Add token to missing endpoints

--- src/api/ExecutionService.ts
+++ src/api/ExecutionService.ts
@@ -8,21 +8,21 @@ const http = axios.create({
     },
 });
 
-export async function getAllExecutions(page: string, size: string, approved: string): Promise<any> {
+export async function getAllExecutions(from: string, size: string, approved: string): Promise<any> {
     http.defaults.headers.common["Authorization"] = `Bearer ${Vue.$keycloak.token}`;
-    const response = await http.get(`/executions?page=${page}&size=${size}&approved=${approved}`);
+    const response = await http.get(`/executions?from=${from}&size=${size}&approved=${approved}`);
     return response.data;
 }
 
 export async function getModelExecutions(
     model_id: string,
-    page: string,
+    from: string,
     size: string,
     approved: string,
 ): Promise<any> {
     http.defaults.headers.common["Authorization"] = `Bearer ${Vue.$keycloak.token}`;
     const response = await http.get(
-        `/executions?model_id=${model_id}&page=${page}&size=${size}&approved=${approved}`,
+        `/executions?model_id=${model_id}&from=${from}&size=${size}&approved=${approved}`,
     );
     return response.data;
 }
@@ -34,13 +34,13 @@ export async function getExecutionPipelines(collaboration_uid: string): Promise<
 }
 
 export async function getExecutionStats(days: string): Promise<any> {
-    http.defaults.headers.common["Authorization"] = Vue.$keycloak.token;
+    http.defaults.headers.common["Authorization"] = `Bearer ${Vue.$keycloak.token}`;
     const response = await http.get(`/execution_stats?days=${days}`);
     return response.data;
 }
 
 export async function getModelExecutionStats(days: string, model_id: string): Promise<any> {
-    http.defaults.headers.common["Authorization"] = Vue.$keycloak.token;
+    http.defaults.headers.common["Authorization"] = `Bearer ${Vue.$keycloak.token}`;
     const response = await http.get(`/execution_stats?days=${days}&model_id=${model_id}`);
     return response.data;
 }
