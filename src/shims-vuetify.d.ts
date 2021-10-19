eea566ab019d4ebf14a0d92ea1674413fb8aad4d
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2021-10-19 09:32:31 +0100

    Add module declarations

--- src/shims-vuetify.d.ts
+++ src/shims-vuetify.d.ts
@@ -7,3 +7,10 @@ declare module "vue-json-pretty" {
     function VueJsonPretty(): void;
     export = VueJsonPretty;
 }
+
+declare module "vue-pdf";
+declare module "cornerstone-core";
+declare module "cornerstone-web-image-loader";
+declare module "cornerstone-tools";
+declare module "cornerstone-math";
+declare module "hammerjs";
