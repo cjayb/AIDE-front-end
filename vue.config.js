16c78d725750167911bdc66bd95f9a36b92ec433
Sam Rooke <sam.rooke@answerdigital.com>
2022-10-21 15:30:12 +0100

    AC-658: add alias for keycloak plugin

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
+const { join } = require("path");
 
 module.exports = defineConfig({
     transpileDependencies: ["vuetify"],
     configureWebpack: {
+        resolve: {
+            alias: {
+                "keycloak-js": join(__dirname, "node_modules/keycloak-js"),
+            },
+        },
         experiments: {
             topLevelAwait: process.env.NODE_ENV === "development",
         },
